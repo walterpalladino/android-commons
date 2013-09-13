131cd7601c19ce8be51babcc6ce27fb5fda55bdf
walterpalladino <wpalladino@gmail.com>
2013-09-13 17:08:32 -0300

    Documentation updated Added functionality to FontUtils

--- AndroidCommons/src/com/whp/android/fonts/FontUtils.java
+++ AndroidCommons/src/com/whp/android/fonts/FontUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Walter Hugo Palladino
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.whp.android.fonts;
 
 import java.util.EnumMap;
@@ -5,51 +20,44 @@ import java.util.Map;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-/*
-
- And the last thing in your onCreate main activity:
- if (Build.VERSION.SDK_INT < 11) {
- ViewGroup godfatherView = (ViewGroup) this.getWindow().getDecorView();
- FontUtils.setRobotoFont(this, godfatherView);
- }
-
- And in my lists with custom views the code above didn't work, so I had to make this:
- @Override
- public View getView(int position, View convertView, ViewGroup parent) {
- // (...)
-
- View view = // build your custom view here
-
- if (Build.VERSION.SDK_INT < 11) {
- FontUtils.setRobotoFont(activity, view);
- }
-
- return view;
- }
-
+/**
+ * @author Walter Hugo Palladino
+ * @since 13/09/2013
+ * 
  */
 public final class FontUtils {
 
+	/**
+	 * Property Name : DEFAULT_FONT_FACE
+	 * 
+	 */
 	private static final String DEFAULT_FONT_FACE = "fonts/Roboto_Light.ttf";
 
-	private FontUtils() {
+	/**
+	 * Constructor
+	 */
+	private FontUtils () {
 	}
 
+	/**
+	 * @author Walter Hugo Palladino
+	 * @since 13/09/2013
+	 * 
+	 */
 	private enum FontType {
-		BOLD("fonts/Roboto/Roboto-BoldCondensed.ttf"), BOLD_ITALIC(
-				"fonts/Roboto/Roboto-BoldCondensedItalic.ttf"), NORMAL(
-				"fonts/Roboto/Roboto-Condensed.ttf"), ITALIC(
-				"fonts/Roboto/Roboto-CondensedItalic.ttf");
+		BOLD("fonts/Roboto/Roboto-BoldCondensed.ttf"), BOLD_ITALIC("fonts/Roboto/Roboto-BoldCondensedItalic.ttf"), NORMAL(
+				"fonts/Roboto/Roboto-Condensed.ttf"), ITALIC("fonts/Roboto/Roboto-CondensedItalic.ttf");
 
 		private final String path;
 
-		FontType(String path) {
+		FontType (String path) {
 			this.path = path;
 		}
 
@@ -58,37 +66,44 @@ public final class FontUtils {
 		}
 	}
 
-	/* cache for loaded Roboto typefaces */
-	private static Map<FontType, Typeface> typefaceCache = new EnumMap<FontType, Typeface>(
-			FontType.class);
+	/**
+	 * Property Name : typefaceCache
+	 * 
+	 */
+	private static Map <FontType, Typeface> typefaceCache = new EnumMap <FontType, Typeface> (FontType.class);
 
 	/**
-	 * Creates Roboto typeface and puts it into cache
+	 * getRobotoTypeface : Creates Roboto typeface and puts it into cache
+	 * 
+	 * @param context
+	 * @param fontType
+	 * @return
 	 */
 	private static Typeface getRobotoTypeface(Context context, FontType fontType) {
-		String fontPath = fontType.getPath();
+		String fontPath = fontType.getPath ();
 
-		if (!typefaceCache.containsKey(fontType)) {
-			typefaceCache.put(fontType,
-					Typeface.createFromAsset(context.getAssets(), fontPath));
+		if (!typefaceCache.containsKey (fontType)) {
+			typefaceCache.put (fontType, Typeface.createFromAsset (context.getAssets (), fontPath));
 		}
 
-		return typefaceCache.get(fontType);
+		return typefaceCache.get (fontType);
 	}
 
 	/**
-	 * Gets roboto typeface according to passed typeface style settings.
-	 * <p/>
-	 * Will get Roboto-Bold for Typeface.BOLD etc
+	 * getRobotoTypeface : Gets roboto typeface according to passed typeface
+	 * style settings.
+	 * 
+	 * @param context
+	 * @param originalTypeface
+	 * @return
 	 */
-	private static Typeface getRobotoTypeface(Context context,
-			Typeface originalTypeface) {
+	private static Typeface getRobotoTypeface(Context context, Typeface originalTypeface) {
 		FontType robotoFontType = null;
 
 		if (originalTypeface == null) {
 			robotoFontType = FontType.NORMAL;
 		} else {
-			int style = originalTypeface.getStyle();
+			int style = originalTypeface.getStyle ();
 
 			switch (style) {
 			case Typeface.BOLD:
@@ -109,8 +124,7 @@ public final class FontUtils {
 			}
 		}
 
-		return (robotoFontType == null) ? originalTypeface : getRobotoTypeface(
-				context, robotoFontType);
+		return (robotoFontType == null) ? originalTypeface : getRobotoTypeface (context, robotoFontType);
 	}
 
 	/**
@@ -124,37 +138,59 @@ public final class FontUtils {
 	 */
 	public static void setRobotoFont(Context context, View view) {
 		if (view instanceof ViewGroup) {
-			for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
-				setRobotoFont(context, ((ViewGroup) view).getChildAt(i));
+			for (int i = 0; i < ((ViewGroup) view).getChildCount (); i++) {
+				setRobotoFont (context, ((ViewGroup) view).getChildAt (i));
 			}
 		} else if (view instanceof TextView) {
-			Typeface currentTypeface = ((TextView) view).getTypeface();
-			((TextView) view).setTypeface(getRobotoTypeface(context,
-					currentTypeface));
+			Typeface currentTypeface = ((TextView) view).getTypeface ();
+			((TextView) view).setTypeface (getRobotoTypeface (context, currentTypeface));
 		}
 	}
 
+	/**
+	 * updateFonts
+	 * 
+	 * @param context
+	 * @param root
+	 * @param force
+	 */
+	public static void updateFonts(Context context, ViewGroup root, boolean force) {
+		if (force) {
+			updateFonts (context, root);
+		} else {
+			if (Build.VERSION.SDK_INT < 11) {
+				updateFonts (context, root);
+			}
+
+		}
+	}
+
+	/**
+	 * updateFonts
+	 * 
+	 * @param context
+	 * @param root
+	 */
 	public static void updateFonts(Context context, ViewGroup root) {
 
-		Typeface tf = Typeface.createFromAsset(context.getAssets(),
-				DEFAULT_FONT_FACE);
+		Typeface tf = Typeface.createFromAsset (context.getAssets (), DEFAULT_FONT_FACE);
 
-		for (int i = 0; i < root.getChildCount(); i++) {
-			View v = root.getChildAt(i);
+		for (int i = 0; i < root.getChildCount (); i++) {
+			View v = root.getChildAt (i);
 			if (v instanceof TextView) {
-				((TextView) v).setTypeface(tf);
+				((TextView) v).setTypeface (tf);
 			} else if (v instanceof EditText) {
-				((EditText) v).setTypeface(tf);
+				((EditText) v).setTypeface (tf);
 			} else if (v instanceof Button) {
-				((Button) v).setTypeface(tf);
+				((Button) v).setTypeface (tf);
 			} else if (v instanceof android.widget.RelativeLayout) {
-				FontUtils.updateFonts(context, (ViewGroup) v);
+				FontUtils.updateFonts (context, (ViewGroup) v);
 			} else if (v instanceof android.widget.FrameLayout) {
-				FontUtils.updateFonts(context, (ViewGroup) v);
+				FontUtils.updateFonts (context, (ViewGroup) v);
 			} else if (v instanceof android.widget.LinearLayout) {
-				FontUtils.updateFonts(context, (ViewGroup) v);
+				FontUtils.updateFonts (context, (ViewGroup) v);
 			} else if (v instanceof android.widget.ScrollView) {
-				FontUtils.updateFonts(context, (ViewGroup) v);
+				FontUtils.updateFonts (context, (ViewGroup) v);
 			}
 
 			/*
