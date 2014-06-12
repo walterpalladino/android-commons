13cfe1661e0624a5d5a5db98fe19b05a97dc5110
walterpalladino <wpalladino@gmail.com>
2014-06-12 12:58:10 -0300

    Added SecurityUtils
    Some methods for encoding / decoding

--- AndroidCommons/src/com/whp/android/security/SecurityUtils.java
+++ AndroidCommons/src/com/whp/android/security/SecurityUtils.java
@@ -15,8 +15,20 @@
  */
 package com.whp.android.security;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import android.content.Context;
 import android.content.pm.PackageInfo;
@@ -60,5 +72,134 @@ public class SecurityUtils {
 			Log.e ("Exception", e.toString ());
 		}
 		return hash;
+	}
+
+	/**
+	 * Encodes a String in AES-128 with a given key
+	 * 
+	 * @param context
+	 * @param password
+	 * @param text
+	 * @return String Base64 and AES encoded String
+	 */
+	public static String encode(Context context, String password, String text) {
+		if (password.length () == 0 || password == null) {
+			return null;
+		}
+
+		if (text.length () == 0 || text == null) {
+			return null;
+		}
+
+		try {
+			SecretKeySpec skeySpec = getKey (password);
+			byte [] clearText = text.getBytes ("UTF8");
+
+			// IMPORTANT TO GET SAME RESULTS ON iOS and ANDROID
+			final byte [] iv = new byte [16];
+			Arrays.fill (iv, (byte) 0x00);
+			IvParameterSpec ivParameterSpec = new IvParameterSpec (iv);
+
+			// Cipher is not thread safe
+			Cipher cipher = Cipher.getInstance ("AES/CBC/PKCS7Padding");
+			cipher.init (Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
+			
+			String encrypedValue = Base64.encodeToString (cipher.doFinal (clearText), Base64.NO_WRAP);
+
+			return encrypedValue;
+
+		} catch (InvalidKeyException e) {
+			e.printStackTrace ();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace ();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace ();
+		} catch (BadPaddingException e) {
+			e.printStackTrace ();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace ();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace ();
+		} catch (InvalidAlgorithmParameterException e) {
+			e.printStackTrace ();
+		}
+		return null;
+	}
+
+	/**
+	 * Decodes a String using AES-128 and Base64
+	 * 
+	 * @param context
+	 * @param password
+	 * @param text
+	 * @return decoded String
+	 */
+	public static String decode(Context context, String password, String text) {
+
+		if (password.length () == 0 || password == null) {
+			return null;
+		}
+
+		if (text.length () == 0 || text == null) {
+			return null;
+		}
+
+		try {
+			SecretKey key = getKey (password);
+
+			// IMPORTANT TO GET SAME RESULTS ON iOS and ANDROID
+			final byte [] iv = new byte [16];
+			Arrays.fill (iv, (byte) 0x00);
+			IvParameterSpec ivParameterSpec = new IvParameterSpec (iv);
+
+			byte [] encrypedPwdBytes = Base64.decode (text, Base64.NO_WRAP);
+			// cipher is not thread safe
+			Cipher cipher = Cipher.getInstance ("AES/CBC/PKCS7Padding");
+			cipher.init (Cipher.DECRYPT_MODE, key, ivParameterSpec);
+			byte [] decrypedValueBytes = (cipher.doFinal (encrypedPwdBytes));
+
+			String decrypedValue = new String (decrypedValueBytes);
+
+			return decrypedValue;
+
+		} catch (InvalidKeyException e) {
+			e.printStackTrace ();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace ();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace ();
+		} catch (BadPaddingException e) {
+			e.printStackTrace ();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace ();
+		} catch (IllegalBlockSizeException e) {
+			e.printStackTrace ();
+		} catch (InvalidAlgorithmParameterException e) {
+			e.printStackTrace ();
+		}
+		return "";
+	}
+
+	/**
+	 * Generates a SecretKeySpec for given password
+	 * 
+	 * @param password
+	 * @return SecretKeySpec
+	 * @throws UnsupportedEncodingException
+	 */
+	public static SecretKeySpec getKey(String password) throws UnsupportedEncodingException {
+
+		int keyLength = 128;
+		byte [] keyBytes = new byte [keyLength / 8];
+		// explicitly fill with zeros
+		Arrays.fill (keyBytes, (byte) 0x0);
+
+		// if password is shorter then key length, it will be zero-padded
+		// to key length
+		byte [] passwordBytes = password.getBytes ("UTF-8");
+		int length = passwordBytes.length < keyBytes.length ? passwordBytes.length : keyBytes.length;
+		System.arraycopy (passwordBytes, 0, keyBytes, 0, length);
+		SecretKeySpec key = new SecretKeySpec (keyBytes, "AES");
+		return key;
 	}
 }
