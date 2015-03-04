a18f8b7d57dc0fe55c05154d715f2503a5da218a
Walter H. Palladino <walterpalladino@Walters-Mac-mini.local>
2015-03-04 07:12:26 -0300

    Added getRoundedShape method

--- AndroidCommons/src/com/whp/android/bitmap/BitmapUtils.java
+++ AndroidCommons/src/com/whp/android/bitmap/BitmapUtils.java
@@ -15,14 +15,14 @@
  */
 package com.whp.android.bitmap;
 
-import java.util.Random;
-
 import android.graphics.Bitmap;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
 import android.view.View;
 
 /**
@@ -38,19 +38,19 @@ public class BitmapUtils {
 	 * @param view
 	 * @return
 	 */
-	public static Bitmap getScreen(View view) {
+	public static Bitmap getScreen (View view) {
 
 		View v = view;
 		// v.setDrawingCacheEnabled (true);
 		//
 		// return v.getDrawingCache ();
 
-		v.setDrawingCacheEnabled (true);
+		v.setDrawingCacheEnabled(true);
 
-		v.buildDrawingCache (true);
-		Bitmap b = Bitmap.createBitmap (v.getDrawingCache ());
+		v.buildDrawingCache(true);
+		Bitmap b = Bitmap.createBitmap(v.getDrawingCache());
 
-		v.setDrawingCacheEnabled (false); // clear drawing cache
+		v.setDrawingCacheEnabled(false); // clear drawing cache
 
 		return b;
 	}
@@ -62,45 +62,45 @@ public class BitmapUtils {
 	 * @param radius
 	 * @return
 	 */
-	public static Bitmap fastBlur(Bitmap sentBitmap, int radius) {
+	public static Bitmap fastBlur (Bitmap sentBitmap, int radius) {
 
-		Bitmap bitmap = sentBitmap.copy (sentBitmap.getConfig (), true);
+		Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
 
 		if (radius < 1) {
 			return (null);
 		}
 
-		int w = bitmap.getWidth ();
-		int h = bitmap.getHeight ();
+		int w = bitmap.getWidth();
+		int h = bitmap.getHeight();
 
-		int [] pix = new int [w * h];
+		int[] pix = new int[w * h];
 
-		bitmap.getPixels (pix, 0, w, 0, 0, w, h);
+		bitmap.getPixels(pix, 0, w, 0, 0, w, h);
 
 		int wm = w - 1;
 		int hm = h - 1;
 		int wh = w * h;
 		int div = radius + radius + 1;
 
-		int r[] = new int [wh];
-		int g[] = new int [wh];
-		int b[] = new int [wh];
+		int r[] = new int[wh];
+		int g[] = new int[wh];
+		int b[] = new int[wh];
 		int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
-		int vmin[] = new int [Math.max (w, h)];
+		int vmin[] = new int[Math.max(w, h)];
 
 		int divsum = (div + 1) >> 1;
 		divsum *= divsum;
-		int dv[] = new int [256 * divsum];
+		int dv[] = new int[256 * divsum];
 		for (i = 0; i < 256 * divsum; i++) {
 			dv[i] = (i / divsum);
 		}
 
 		yw = yi = 0;
 
-		int [][] stack = new int [div] [3];
+		int[][] stack = new int[div][3];
 		int stackpointer;
 		int stackstart;
-		int [] sir;
+		int[] sir;
 		int rbs;
 		int r1 = radius + 1;
 		int routsum, goutsum, boutsum;
@@ -109,12 +109,12 @@ public class BitmapUtils {
 		for (y = 0; y < h; y++) {
 			rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
 			for (i = -radius; i <= radius; i++) {
-				p = pix[yi + Math.min (wm, Math.max (i, 0))];
+				p = pix[yi + Math.min(wm, Math.max(i, 0))];
 				sir = stack[i + radius];
 				sir[0] = (p & 0xff0000) >> 16;
 				sir[1] = (p & 0x00ff00) >> 8;
 				sir[2] = (p & 0x0000ff);
-				rbs = r1 - Math.abs (i);
+				rbs = r1 - Math.abs(i);
 				rsum += sir[0] * rbs;
 				gsum += sir[1] * rbs;
 				bsum += sir[2] * rbs;
@@ -148,7 +148,7 @@ public class BitmapUtils {
 				boutsum -= sir[2];
 
 				if (y == 0) {
-					vmin[x] = Math.min (x + radius + 1, wm);
+					vmin[x] = Math.min(x + radius + 1, wm);
 				}
 				p = pix[yw + vmin[x]];
 
@@ -183,7 +183,7 @@ public class BitmapUtils {
 			rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
 			yp = -radius * w;
 			for (i = -radius; i <= radius; i++) {
-				yi = Math.max (0, yp) + x;
+				yi = Math.max(0, yp) + x;
 
 				sir = stack[i + radius];
 
@@ -191,7 +191,7 @@ public class BitmapUtils {
 				sir[1] = g[yi];
 				sir[2] = b[yi];
 
-				rbs = r1 - Math.abs (i);
+				rbs = r1 - Math.abs(i);
 
 				rsum += r[yi] * rbs;
 				gsum += g[yi] * rbs;
@@ -229,7 +229,7 @@ public class BitmapUtils {
 				boutsum -= sir[2];
 
 				if (x == 0) {
-					vmin[y] = Math.min (y + r1, hm) * w;
+					vmin[y] = Math.min(y + r1, hm) * w;
 				}
 				p = x + vmin[y];
 
@@ -260,35 +260,35 @@ public class BitmapUtils {
 			}
 		}
 
-		bitmap.setPixels (pix, 0, w, 0, 0, w, h);
+		bitmap.setPixels(pix, 0, w, 0, 0, w, h);
 
 		return (bitmap);
 	}
 
-	public static Bitmap blurFilter(Bitmap src, float blurValue) {
-		int width = src.getWidth ();
-		int height = src.getHeight ();
+	public static Bitmap blurFilter (Bitmap src, float blurValue) {
+		int width = src.getWidth();
+		int height = src.getHeight();
 
 		BlurMaskFilter blurMaskFilter;
-		Paint paintBlur = new Paint ();
+		Paint paintBlur = new Paint();
 
-		Bitmap dest = Bitmap.createBitmap (width, height, Bitmap.Config.RGB_565);
-		Canvas canvas = new Canvas (dest);
+		Bitmap dest = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+		Canvas canvas = new Canvas(dest);
 
 		// Create background in White
-		Bitmap alpha = src.extractAlpha ();
-		paintBlur.setColor (0xFFFFFFFF);
-		canvas.drawBitmap (alpha, 0, 0, paintBlur);
+		Bitmap alpha = src.extractAlpha();
+		paintBlur.setColor(0xFFFFFFFF);
+		canvas.drawBitmap(alpha, 0, 0, paintBlur);
 
 		// Create outer blur, in White
-		blurMaskFilter = new BlurMaskFilter (blurValue, BlurMaskFilter.Blur.OUTER);
-		paintBlur.setMaskFilter (blurMaskFilter);
-		canvas.drawBitmap (alpha, 0, 0, paintBlur);
+		blurMaskFilter = new BlurMaskFilter(blurValue, BlurMaskFilter.Blur.OUTER);
+		paintBlur.setMaskFilter(blurMaskFilter);
+		canvas.drawBitmap(alpha, 0, 0, paintBlur);
 
 		// Create inner blur
-		blurMaskFilter = new BlurMaskFilter (blurValue, BlurMaskFilter.Blur.INNER);
-		paintBlur.setMaskFilter (blurMaskFilter);
-		canvas.drawBitmap (src, 0, 0, paintBlur);
+		blurMaskFilter = new BlurMaskFilter(blurValue, BlurMaskFilter.Blur.INNER);
+		paintBlur.setMaskFilter(blurMaskFilter);
+		canvas.drawBitmap(src, 0, 0, paintBlur);
 
 		return dest;
 	}
@@ -299,13 +299,13 @@ public class BitmapUtils {
 	 * @param src
 	 * @return
 	 */
-	public static Bitmap applyGaussianBlur(Bitmap src) {
-		double [][] GaussianBlurConfig = new double [] [] { { 1, 2, 1 }, { 2, 4, 2 }, { 1, 2, 1 } };
-		ConvolutionMatrix convMatrix = new ConvolutionMatrix (3);
-		convMatrix.applyConfig (GaussianBlurConfig);
+	public static Bitmap applyGaussianBlur (Bitmap src) {
+		double[][] GaussianBlurConfig = new double[][] { { 1, 2, 1 }, { 2, 4, 2 }, { 1, 2, 1 } };
+		ConvolutionMatrix convMatrix = new ConvolutionMatrix(3);
+		convMatrix.applyConfig(GaussianBlurConfig);
 		convMatrix.Factor = 16;
 		convMatrix.Offset = 0;
-		return ConvolutionMatrix.computeConvolution3x3 (src, convMatrix);
+		return ConvolutionMatrix.computeConvolution3x3(src, convMatrix);
 	}
 
 	/**
@@ -314,13 +314,13 @@ public class BitmapUtils {
 	 * @param source
 	 * @return
 	 */
-	public static Bitmap applyDarkFilter(Bitmap source, float factor) {
+	public static Bitmap applyDarkFilter (Bitmap source, float factor) {
 		// get image source size
-		int width = source.getWidth ();
-		int height = source.getHeight ();
-		int [] pixels = new int [width * height];
+		int width = source.getWidth();
+		int height = source.getHeight();
+		int[] pixels = new int[width * height];
 		// get pixel array from source
-		source.getPixels (pixels, 0, width, 0, 0, width, height);
+		source.getPixels(pixels, 0, width, 0, 0, width, height);
 
 		int R, G, B, index = 0;
 		// iteration through pixels
@@ -329,17 +329,17 @@ public class BitmapUtils {
 				// get current index in 2D-matrix
 				index = y * width + x;
 				// get RGB colors
-				R = Color.red (pixels[index]);
-				G = Color.green (pixels[index]);
-				B = Color.blue (pixels[index]);
-				
-				pixels[index] = Color.rgb ((int) (R*factor), (int) (G*factor), (int) (B*factor));
-				
+				R = Color.red(pixels[index]);
+				G = Color.green(pixels[index]);
+				B = Color.blue(pixels[index]);
+
+				pixels[index] = Color.rgb((int) (R * factor), (int) (G * factor), (int) (B * factor));
+
 			}
 		}
 		// create output bitmap
-		Bitmap bmOut = Bitmap.createBitmap (width, height, Bitmap.Config.ARGB_8888);
-		bmOut.setPixels (pixels, 0, width, 0, 0, width, height);
+		Bitmap bmOut = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		bmOut.setPixels(pixels, 0, width, 0, 0, width, height);
 		return bmOut;
 	}
 
@@ -351,18 +351,18 @@ public class BitmapUtils {
 	 * @param newWidth
 	 * @return
 	 */
-	public static Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
-		int width = bm.getWidth ();
-		int height = bm.getHeight ();
+	public static Bitmap getResizedBitmap (Bitmap bm, int newHeight, int newWidth) {
+		int width = bm.getWidth();
+		int height = bm.getHeight();
 		float scaleWidth = ((float) newWidth) / width;
 		float scaleHeight = ((float) newHeight) / height;
 		// CREATE A MATRIX FOR THE MANIPULATION
-		Matrix matrix = new Matrix ();
+		Matrix matrix = new Matrix();
 		// RESIZE THE BIT MAP
-		matrix.postScale (scaleWidth, scaleHeight);
+		matrix.postScale(scaleWidth, scaleHeight);
 
 		// "RECREATE" THE NEW BITMAP
-		Bitmap resizedBitmap = Bitmap.createBitmap (bm, 0, 0, width, height, matrix, false);
+		Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
 		return resizedBitmap;
 	}
 
@@ -372,11 +372,33 @@ public class BitmapUtils {
 	 * @param bm
 	 * @return
 	 */
-	public static Bitmap getHalvedBitmap(Bitmap bm) {
-		int newWidth = bm.getWidth () / 2;
-		int newHeight = bm.getHeight () / 2;
-		//return BitmapUtils.getResizedBitmap (bm, newHeight, newWidth);
+	public static Bitmap getHalvedBitmap (Bitmap bm) {
+		int newWidth = bm.getWidth() / 2;
+		int newHeight = bm.getHeight() / 2;
+		// return BitmapUtils.getResizedBitmap (bm, newHeight, newWidth);
 		return Bitmap.createScaledBitmap(bm, newWidth, newHeight, false);
+	}
+
+	/**
+	 * getRoundedShape
+	 * 
+	 * @param scaleBitmapImage
+	 * @return
+	 */
+	public static Bitmap getRoundedShape (Bitmap scaleBitmapImage, int targetWidth, int targetHeight) {
+
+		Bitmap targetBitmap = Bitmap.createBitmap(targetWidth, targetHeight, Bitmap.Config.ARGB_8888);
+
+		Canvas canvas = new Canvas(targetBitmap);
+		Path path = new Path();
+		path.addCircle(((float) targetWidth - 1) / 2, ((float) targetHeight - 1) / 2,
+				(Math.min(((float) targetWidth), ((float) targetHeight)) / 2), Path.Direction.CCW);
+
+		canvas.clipPath(path);
+		Bitmap sourceBitmap = scaleBitmapImage;
+		canvas.drawBitmap(sourceBitmap, new Rect(0, 0, sourceBitmap.getWidth(), sourceBitmap.getHeight()), new Rect(0, 0,
+				targetWidth, targetHeight), null);
+		return targetBitmap;
 	}
 
 }
